a3c9339d81fa1e89a802099deb614ac5dd9c545e
Gourav Khunger <gouravkhunger18@gmail.com>
2024-07-01 11:10:21 +0530

    chore: update package name

---
+++ app/src/main/java/xyz/jekyllex/app/packagemanager/Constants.kt
@@ -0,0 +1,9 @@
+package xyz.jekyllex.app.packagemanager
+
+class Constants {
+    companion object {
+        const val PACKAGE_INSTALLED_ACTION = "xyz.jekyllex.SESSION_API_PACKAGE_INSTALLED"
+        const val TERMUX_CACHE_PKG_DIRECTORY_SUBFOLDER = "/pkg"
+        const val APK_REPO_URL = "https://termux.net/apks/"
+    }
+}
